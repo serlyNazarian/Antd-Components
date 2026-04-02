a630d9c2c787a1982af86602c774a775c4581cf0
Serly Nazarian <serlynazarian@gmail.com>
2026-04-02 14:34:03 +0400

    comment notification section

--- src/components/myButtonCopy/MyButtonCopy.jsx
+++ src/components/myButtonCopy/MyButtonCopy.jsx
@@ -1,11 +1,11 @@
 import { CopyOutlined } from '@ant-design/icons';
 import MyButtonSecondary from '../myButton/MyButtonSecondary';
-import { NotificationService } from '../../utils/NotificationService';
+// import { NotificationService } from '../../utils/NotificationService';
 
 const MyButtonCopy = ({ label }) => {
   const handleCopy = () => {
     navigator.clipboard.writeText(label).then(() => {
-      NotificationService.UtilNotify.notifyInfo('WORD_CODE_COPIED');
+      // NotificationService.UtilNotify.notifyInfo('WORD_CODE_COPIED');
     });
   };
 
